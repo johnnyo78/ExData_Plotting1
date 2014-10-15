a8afdcddfa349941c0e6f12d530799f9b12019e9
johnnyo78 <johnnyo78@users.noreply.github.com>
2014-10-14 19:57:00 -0700

    Commit R scripts and png outputs for assignment 1

---
+++ plot2.R
@@ -0,0 +1,20 @@
+###
+### Course Assignment #1: plot2.R
+###
+#install.packages("sqldf");
+library(sqldf);
+
+#Import the range of interest from the datafile
+datafile <- "./data/household_power_consumption.txt";
+cls <- c(Global_active_power="numeric", Global_reactive_power="numeric", Voltage="numeric", Global_intensity="numeric");
+indata <- read.csv.sql(datafile, sep=";", header=T, sql = "SELECT * from file WHERE Date IN ('1/2/2007','2/2/2007')", colClasses=cls);
+indata$DateTime <- strptime(paste(indata$Date, indata$Time), "%d/%m/%Y %H:%M:%S");
+indata$DayOfWeek <- weekdays(indata$DateTime, abbreviate=T); # Not needed, but useful for correlating data w graphs
+
+#Plot the information
+plot(indata$DateTime, indata$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", typ="l");
+
+#Copy to PNG format and close the device
+dev.copy(png, filename="plot2.png", width=480, height=480, units="px");
+dev.off();
+
